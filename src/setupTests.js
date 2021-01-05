53dbc395f62d9f3126ab659d272ba5d80850a561
fabianmolinab <molinafabian11b@gmail.com>
2021-01-04 20:25:11 -0500

    Realizando los test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,8 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
 
-import '@testing-library/jest-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
